4c63b37803d81e0f21a8374c0c1c19ad2aedf79e
Vikas Kumar Singh <vikaskumarsingh0931@gmail.com>
2023-04-14 15:07:42 +0530

    Add files via upload

---
+++ blogs-context-starter/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full border shadow-sm py-2 fixed scroll-m-7 top-0 bg-white '>
+      <header className='text-center' >
+        <h1 className='text-xl font-bold uppercase'>My Blogs</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Header
